43bad0a8d6b9afec1118210e7b1e12fb368724e6
tbarrett1200 <tbarrett1200@icloud.com>
2016-09-11 13:24:14 -0400

    Add

---
+++ DeliDash/CheckBox.swift
@@ -0,0 +1,50 @@
+//
+//  checkBox.swift
+//  DeliDash
+//
+//  Created by Alexander Leininger on 5/31/16.
+//  Copyright © 2016 Alexander Leininger. All rights reserved.
+//
+
+import UIKit
+
+class CheckBox: UIButton {
+
+    //images
+    let checkedImage = UIImage(named: "checked")
+    let uncheckedImage = UIImage(named: "unchecked")
+    
+    //Bool Property
+    var isChecked:Bool = false{
+
+    didSet{
+        if isChecked == true{
+            self.setImage(checkedImage, forState: .Normal)
+            }
+        else{
+            self.setImage(uncheckedImage, forState: .Normal)
+            }
+        }
+    }
+    override func awakeFromNib() {
+        self.addTarget(self, action: #selector(CheckBox.buttonClicked(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+            self.isChecked = false
+    }
+
+    func buttonClicked(sender:UIButton){
+        
+        if(sender == self)
+        {
+            if isChecked == true
+            {
+                
+                isChecked = false
+                
+            }
+            else
+            {
+                isChecked = true
+            }
+        }
+    }
+}
